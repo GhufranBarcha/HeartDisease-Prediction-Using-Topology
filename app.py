5d5892b74a7d840b0ee60fdaf22c899eddeff278
GhufranBarcha <gufoor.barcha2906@gmail.com>
2023-10-25 15:10:21 +0500

    Updated app.py

--- app.py
+++ app.py
@@ -252,11 +252,11 @@ if selected == "Research Paper":
             def show_pdf(file_path):
                 with open(file_path,"rb") as f:
                     base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-                    st.markdown("""
-                             <embed src="https://thomasmorestudies.org/wp-content/uploads/2020/09/Richard.pdf" width="800" height="800">
-                         """, unsafe_allow_html=True)    
-                # pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="800" height="800" type="application/pdf"></iframe>'
-                # st.markdown(pdf_display, unsafe_allow_html=True)    
+                    # st.markdown("""
+                    #          <embed src="https://thomasmorestudies.org/wp-content/uploads/2020/09/Richard.pdf" width="800" height="800">
+                    #      """, unsafe_allow_html=True)    
+                pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="800" height="800" type="application/pdf"></iframe>'
+                st.markdown(pdf_display, unsafe_allow_html=True)    
             show_pdf('Applications_of_Topology_to_the_Detection_of_Ventricular_Tachycardia_Zhang_Tumuluri.pdf')
             
 
